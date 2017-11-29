be7ea9a3247e156ef0ab7b9f1bd124a19a8fd451
Valera Yalovchuk <valera.yalow4uk@gmail.com>
2017-11-30 00:03:57 +0300

    dev: fix getAllUsers endpoint to public

--- backend/business/src/main/java/com/perfect/team/business/auth/security/WebSecurityConfig.java
+++ backend/business/src/main/java/com/perfect/team/business/auth/security/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/api/v1/auth/**");
+        web.ignoring().antMatchers("/api/v1/auth/**", "/api/v1/users");
     }
 
     @Override
