8ae06e3c04a1a1f2722d7257691dca6d5baea58e
Valera <valera.yalow4uk@gmail.com>
2018-01-14 22:46:07 +0300

    dev: fix sql

---
+++ db/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO category_type (name, balance_type) VALUES ('', 'INCOME');
+INSERT INTO category_type (name, balance_type) VALUES ('', 'OUTCOME');
--- db/dump.sql
+++ db/dump.sql
@@ -12,13 +12,15 @@ CREATE TABLE `user` (
 CREATE TABLE `account` (
   `id`           BIGINT(20) NOT NULL AUTO_INCREMENT,
   `icon`         VARCHAR(255)        DEFAULT NULL,
-  `balance`      BIGINT(20)          DEFAULT NULL,
+  `balance`      DECIMAL(10, 2)      DEFAULT NULL,
   `initial_date` DATETIME            DEFAULT NULL,
   `name`         VARCHAR(255)        DEFAULT NULL,
+  `currency`     VARCHAR(255),
   `user_id`      BIGINT(20)          DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK7m8ru44m93ukyb61dfxw0apf6` (`user_id`),
   CONSTRAINT `FK7m8ru44m93ukyb61dfxw0apf6` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+    ON DELETE CASCADE
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
@@ -40,9 +42,11 @@ CREATE TABLE `category` (
   `category_type_id` BIGINT(20)          DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FKpfk8djhv5natgshmxiav6xkpu` (`user_id`),
-  KEY `FKpfk8djhv5natgshmxiav6xkpu` (`category_type_id`),
-  CONSTRAINT `FKpfk8djhv5natgshmxiav6xkpu` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `FKpfk8djhv5natgshmxiav6xkpu` FOREIGN KEY (`category_type_id`) REFERENCES `category_type` (`id`)
+  KEY `FKpfk8djhv5natqshmxiav6xkpu` (`category_type_id`),
+  CONSTRAINT `FKpfk8djhv5natgshmxiav6xkpu` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+    ON DELETE CASCADE,
+  CONSTRAINT `FKpfk8djhv5natqshmxiav6xkpu` FOREIGN KEY (`category_type_id`) REFERENCES `category_type` (`id`)
+    ON DELETE CASCADE
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
@@ -51,7 +55,7 @@ CREATE TABLE `transaction` (
   `id`          BIGINT(20) NOT NULL AUTO_INCREMENT,
   `date`        DATETIME            DEFAULT NULL,
   `note`        VARCHAR(255)        DEFAULT NULL,
-  `value`       INT(11)             DEFAULT NULL,
+  `value`       DECIMAL(10, 2)      DEFAULT NULL,
   `account_id`  BIGINT(20)          DEFAULT NULL,
   `category_id` BIGINT(20)          DEFAULT NULL,
   `user_id`     BIGINT(20)          DEFAULT NULL,
@@ -59,9 +63,12 @@ CREATE TABLE `transaction` (
   KEY `FK6g20fcr3bhr6bihgy24rq1r1b` (`account_id`),
   KEY `FKgik7ruym8r1n4xngrclc6kiih` (`category_id`),
   KEY `FKsg7jp0aj6qipr50856wf6vbw1` (`user_id`),
-  CONSTRAINT `FK6g20fcr3bhr6bihgy24rq1r1b` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`),
-  CONSTRAINT `FKgik7ruym8r1n4xngrclc6kiih` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
+  CONSTRAINT `FK6g20fcr3bhr6bihgy24rq1r1b` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+    ON DELETE CASCADE,
+  CONSTRAINT `FKgik7ruym8r1n4xngrclc6kiih` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+    ON DELETE CASCADE,
   CONSTRAINT `FKsg7jp0aj6qipr50856wf6vbw1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+    ON DELETE CASCADE
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
@@ -70,7 +77,8 @@ CREATE TABLE `transfer` (
   `id`              BIGINT(20) NOT NULL AUTO_INCREMENT,
   `date`            DATETIME            DEFAULT NULL,
   `note`            VARCHAR(255)        DEFAULT NULL,
-  `value`           INT(11)             DEFAULT NULL,
+  `value`           DECIMAL(10, 2)      DEFAULT NULL,
+  `exchange_rate`   DECIMAL(10, 2)      DEFAULT 1,
   `from_account_id` BIGINT(20)          DEFAULT NULL,
   `to_account_id`   BIGINT(20)          DEFAULT NULL,
   `user_id`         BIGINT(20)          DEFAULT NULL,
@@ -78,9 +86,12 @@ CREATE TABLE `transfer` (
   KEY `FKfbrtpigqywdcc6e64ichki73j` (`from_account_id`),
   KEY `FKrppn0djf9uip7gj308swxq54j` (`to_account_id`),
   KEY `FKddbjk8kle2s7siw04lua30sjl` (`user_id`),
-  CONSTRAINT `FKddbjk8kle2s7siw04lua30sjl` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `FKfbrtpigqywdcc6e64ichki73j` FOREIGN KEY (`from_account_id`) REFERENCES `account` (`id`),
+  CONSTRAINT `FKddbjk8kle2s7siw04lua30sjl` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+    ON DELETE CASCADE,
+  CONSTRAINT `FKfbrtpigqywdcc6e64ichki73j` FOREIGN KEY (`from_account_id`) REFERENCES `account` (`id`)
+    ON DELETE CASCADE,
   CONSTRAINT `FKrppn0djf9uip7gj308swxq54j` FOREIGN KEY (`to_account_id`) REFERENCES `account` (`id`)
+    ON DELETE CASCADE
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
